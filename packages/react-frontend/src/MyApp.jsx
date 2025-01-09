a25f8a8f1147f5c50b8bf4ca77cfc38f767c58e8
Jhammett03 <116196579+Jhammett03@users.noreply.github.com>
2025-01-09 12:34:55 -0800

    Updated Form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -13,13 +13,16 @@ const [characters, setCharacters] = useState([]);
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
   return (
     <div className="container">
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
-      <Form />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
