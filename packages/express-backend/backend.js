38566774b8f64493159b75d8b156cdccf1b45875
Jhammett03 <116196579+Jhammett03@users.noreply.github.com>
2025-01-14 10:32:41 -0800

    Using the POST method

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,6 +45,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -65,6 +70,12 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
 
 app.listen(port, () => {
   console.log(
