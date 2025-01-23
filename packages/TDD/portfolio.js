feddb21b09a3fb03b42efb1e29fdc455cc974e0d
Jhammett03 <116196579+Jhammett03@users.noreply.github.com>
2025-01-23 12:56:41 -0800

    incriment 4

--- packages/TDD/portfolio.js
+++ packages/TDD/portfolio.js
@@ -19,8 +19,22 @@ class Portfolio {
         this.stocks.push({ tickr, shares }); // Add new symbol to portfolio
         }
     }
+
+    sell(tickr, shares) {
+        const existingStock = this.stocks.find(stock => stock.tickr === tickr);
+        if (!existingStock) { // You don't own the stock
+          throw new Error(`Cannot sell: ${tickr} not found in portfolio.`);
+        } 
+        if (shares > existingStock.shares) { // trying to sell more share than you own
+          throw new Error(`Cannot sell: Not enough shares of ${tickr}.`);
+        }
+        existingStock.shares -= shares;
+        if (existingStock.shares === 0) { // if all shares are sold remove the stock
+          this.stocks = this.stocks.filter(stock => stock.tickr !== tickr);
+        }
+      }
+      
 }
 
-  
-  module.exports = Portfolio;
+module.exports = Portfolio;
   
--- packages/TDD/portfolio.test.js
+++ packages/TDD/portfolio.test.js
@@ -15,3 +15,29 @@ test('Make stock purchase', () => {
     portfolio.makePurchase('AMZN', 10)
     expect(portfolio.getStocks()).toEqual([{ tickr: 'AMZN', shares: 10}]);
 });
+
+test('Portfolio should subtract shares when a sale is made', () => {
+    const portfolio = new Portfolio();
+    portfolio.makePurchase('AAPL', 10); // Buy 10 shares
+    portfolio.sell('AAPL', 5); // Sell 5 shares
+    expect(portfolio.getStocks()).toEqual([{ tickr: 'AAPL', shares: 5 }]);
+  });
+  
+  test('Portfolio should remove a symbol if all shares are sold', () => {
+    const portfolio = new Portfolio();
+    portfolio.makePurchase('AAPL', 10); // Buy 10 shares
+    portfolio.sell('AAPL', 10); // Sell all 10 shares
+    expect(portfolio.getStocks()).toEqual([]); // AAPL should be removed
+  });
+  
+  test('Portfolio should throw an error if trying to sell more shares than owned', () => {
+    const portfolio = new Portfolio();
+    portfolio.makePurchase('AAPL', 5); // Buy 5 shares
+    expect(() => portfolio.sell('AAPL', 10)).toThrow(Error);
+  });
+  
+  test('Portfolio should throw an error if trying to sell a non-existent symbol', () => {
+    const portfolio = new Portfolio();
+    expect(() => portfolio.sell('GOOG', 5)).toThrow(Error);
+  });
+  
